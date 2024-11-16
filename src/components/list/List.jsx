ede2ae9ae18d3d86779d964e529fd330aa99912e
Musa Büyükkaba <65968506+Trunday@users.noreply.github.com>
2024-11-17 00:03:14 +0300

    Ufak bir mola Dakika 20.01

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -1,8 +1,13 @@
+import ChatList from "./chatList/chatList"
+import Userinfo from "./userInfo/Userinfo"
 import "./list.css"
 
 const List = () => {
     return (
-        <div className="list">List</div>
+        <div className="list">
+            <Userinfo/>
+            <ChatList/>
+        </div>
     )
 }
 
