3ecda9395b73432455ca9d6d6ae0ce9e9580421a
Musa Büyükkaba <65968506+Trunday@users.noreply.github.com>
2024-11-19 15:40:20 +0300

    27:20

--- src/components/list/chatList/ChatList.jsx
+++ src/components/list/chatList/ChatList.jsx
@@ -1,14 +1,21 @@
+import { useState } from "react";
 import "./chatList.css";
 
 const ChatList = () => {
+  const [addMode, setAddMode] = useState(false);
   return (
     <div className="chatList">
       <div className="search">
         <div className="searchBar">
-            <img src="./search.png" alt="" />
-            <input type="text" placeholder="Ara"/>
+          <img src="./search.png" alt="" />
+          <input type="text" placeholder="Ara" />
         </div>
-        <img src="./plus.png" alt="" />
+        <img
+          src={addMode ? "./minus.png" : "./plus.png"}
+          alt=""
+          className="add"
+          onClick={() => setAddMode((p) => !p)}
+        />
       </div>
     </div>
   );
