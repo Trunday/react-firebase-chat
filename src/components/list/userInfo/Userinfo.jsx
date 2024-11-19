064e0a50bb03a21dec0b6fdee39ea5be2d06ea1d
Musa Büyükkaba <65968506+Trunday@users.noreply.github.com>
2024-11-19 15:01:29 +0300

    Update Userinfo.jsx

--- src/components/list/userInfo/Userinfo.jsx
+++ src/components/list/userInfo/Userinfo.jsx
@@ -5,7 +5,7 @@ const Userinfo = () => {
         <div className="userInfo">
             <div className="user">
                 <img src="./avatar.png" alt="" />
-                <h2>John Doe</h2>
+                <h2>Musa Büyükkaba</h2>
             </div>
             <div className="icons">
                 <img src="./more.png" alt="" />
