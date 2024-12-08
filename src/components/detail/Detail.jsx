978900f19f916602c8df73ec2e863961d0a49741
Musa Büyükkaba <65968506+Trunday@users.noreply.github.com>
2024-12-09 00:06:09 +0300

    52:30

--- src/components/detail/Detail.jsx
+++ src/components/detail/Detail.jsx
@@ -1,9 +1,42 @@
-import "./detail.css"
+import "./detail.css";
 
 const Detail = () => {
-    return (
-        <div className="detail">Detail</div>
-    )
-}
+  return (
+    <div className="detail">
+      <div className="user">
+        <img src="./avatar.png" alt="" />
+        <h2>Musa Büyükkaba</h2>
+        <p>Lorem ipsum, dolor sit amet.</p>
+      </div>
+      <div className="info">
+        <div className="option">
+          <div className="title">
+            <span>Sohbet Ayarları</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Gizlilik & Yardım</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Paylaşılan Resimler</span>
+            <img src="./arrowDown.png" alt="" />
+          </div>
+          <div className="photos"></div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Sohbet Ayarları</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Detail
+export default Detail;
