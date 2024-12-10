b3d60134b8494a94ff212a4817127cfe3c421b30
Musa Büyükkaba <65968506+Trunday@users.noreply.github.com>
2024-12-11 01:45:59 +0300

    1:14:34

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
-import List from "./components/list/List"
-import Chat from "./components/chat/Chat"
-import Detail from "./components/detail/Detail"
+import List from "./components/list/List";
+import Chat from "./components/chat/Chat";
+import Detail from "./components/detail/Detail";
+import Login from "./components/login/login";
 
 const App = () => {
-  return (
-    <div className='container'>
-      <List/>
-      <Chat/>
-      <Detail/>
-    </div>
-  )
-}
+  const user = false;
 
-export default App
+  return (
+    <div className="container">
+      {user ? (
+        <>
+          <List />
+          <Chat />
+          <Detail />
+        </>
+      ) : (
+        <Login />
+      )}
+    </div>
+  );
+};
+
+export default App;
