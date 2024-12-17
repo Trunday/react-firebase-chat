aad7cbba7e3d00573437122be4affff1e5aa7b04
Musa Büyükkaba <65968506+Trunday@users.noreply.github.com>
2024-12-17 22:28:33 +0300

    1:23:54

--- src/components/list/chatList/addUser/AddUser.jsx
+++ src/components/list/chatList/addUser/AddUser.jsx
@@ -7,7 +7,13 @@ const AddUser = () => {
         <input type="text" placeholder="Kullanıcı Adı" name="username" />
         <button>Ara</button>
       </form>
-      <div className="user"></div>
+      <div className="user">
+        <div className="detail">
+          <img src="./avatar.png" alt="" />
+          <span>Musa Büyükkaba</span>
+        </div>
+        <button>Kullanıcı Ekle</button>
+      </div>
     </div>
   );
 };
