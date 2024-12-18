9ab83296f95372f4fff6b6f744b21ef5502389d4
Musa Büyükkaba <65968506+Trunday@users.noreply.github.com>
2024-12-18 22:06:30 +0300

    1:29:26

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: import.meta.env.M_API_KEY,
@@ -15,3 +16,4 @@ const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth();
 export const db = getFirestore();
+export const storage = getStorage();
