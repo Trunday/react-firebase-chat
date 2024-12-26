75979814775b5570e3b87b83c2d13e564a1c1fa6
Musa Büyükkaba <65968506+Trunday@users.noreply.github.com>
2024-12-26 19:33:04 +0300

    2:18:45

--- src/components/list/chatList/addUser/AddUser.jsx
+++ src/components/list/chatList/addUser/AddUser.jsx
@@ -1,19 +1,42 @@
 import "./addUser.css";
+import { db } from "../../../../lib/firebase";
+import { collection, getDocs, query, where } from "firebase/firestore";
+import { useState } from "react";
 
 const AddUser = () => {
+  const [user, setUser] = useState(null);
+
+  const handleSearch = async (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const username = formData.get("username");
+    try {
+      const userRef = collection(db, "users");
+      const q = query(userRef, where("username", "==", username));
+      const querySnapshot = await getDocs(q);
+      
+      if (!querySnapshot.empty) {
+        //TODO Github'da bu kısım güncellenmiş. Tam kod üzerinde kotrol edilmeli. 
+        setUser(querySnapshot.docs[0].data());
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <div className="adduser">
-      <form>
+      <form onSubmit={handleSearch}>
         <input type="text" placeholder="Kullanıcı Adı" name="username" />
         <button>Ara</button>
       </form>
-      <div className="user">
+      {user && <div className="user">
         <div className="detail">
-          <img src="./avatar.png" alt="" />
-          <span>Musa Büyükkaba</span>
+          <img src={user.avatar || "./avatar.png"} alt="" />
+          <span>{user.username}</span>
         </div>
         <button>Kullanıcı Ekle</button>
-      </div>
+      </div>}
     </div>
   );
 };
