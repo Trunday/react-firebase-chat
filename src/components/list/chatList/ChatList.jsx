49e056bc0e627f8749326153050b10b058005bc1
Musa Büyükkaba <65968506+Trunday@users.noreply.github.com>
2025-01-07 23:49:26 +0300

    Update ChatList.jsx

--- src/components/list/chatList/ChatList.jsx
+++ src/components/list/chatList/ChatList.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import "./chatList.css";
 import AddUser from "./addUser/AddUser";
 import { useUserStore } from "../../../lib/userStore";
-import { doc, getDoc, onSnapshot } from "firebase/firestore";
+import { doc, getDoc, onSnapshot, updateDoc } from "firebase/firestore";
 import { db } from "../../../lib/firebase";
 import { useChatStore } from "../../../lib/chatStore";
 
@@ -37,7 +37,24 @@ const ChatList = () => {
   }, [currentUser.id]);
 
   const handleSelect = async (chat) => {
-    changeChat(chat.chatId, chat.user);
+    const userChats = chats.map((item) => {
+      const { user, ...rest } = item;
+      return rest;
+    });
+    const chatIndex = userChats.findIndex(
+      (item) => item.chatId === chat.chatId
+    );
+    userChats[chatIndex].isSeen = true;
+
+    const userChatsRef = doc(db, "userchats", currentUser.id);
+    try {
+      await updateDoc(userChatsRef, {
+        chats: userChats,
+      });
+      changeChat(chat.chatId, chat.user);
+    } catch (err) {
+      console.error(err);
+    }
   };
 
   return (
@@ -60,6 +77,9 @@ const ChatList = () => {
           key={chat.chatId}
           onClick={() => {
             handleSelect(chat);
+          }}
+          style={{
+            backgroundColor: chat?.isSeen ? "transparent" : "#5183fe",
           }}
         >
           <img src={chat.user.avatar || "./avatar.png"} alt="" />
